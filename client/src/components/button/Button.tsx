993ff65d0acc3b89e16484c324d1914defd33d32
vipin-2023 <githublogin321@gmail.com>
2023-08-20 20:05:23 +0530

    user styling

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -1,25 +1,3 @@
-// import { FC,MouseEvent } from 'react';
-// import './button.css';
-
-// interface ButtonProps {
-//   text: string;
-//   isFilled?: boolean;
-//   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-//   type?: 'button' | 'submit' | 'reset'; 
-// }
-
-// const Button: FC<ButtonProps> = ({ text, isFilled = true ,onClick, type = 'button' }) => {
-//   return (
-//     <button
-//     type={type}
-//       className={isFilled ? 'fill-button' : 'outlined-button'}
-//       onClick={onClick}>
-//       {text}
-//     </button>
-//   );
-// };
-
-// export default Button;
 
 import React, { FC, MouseEvent } from 'react';
 import './button.css';
